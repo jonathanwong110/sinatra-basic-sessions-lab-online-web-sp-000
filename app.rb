2670aecf6c812535b7e694159f7465babc8dcd83
Jonathan Wong <jonathan.wong110@gmail.com>
2019-07-26 00:41:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,9 +11,8 @@ class App < Sinatra::Base
   end
   
   post '/checkout' do
-    @sessions = session
-    item = params["item"]
     @sessions[:item] = item
+    @sessions = session
   end
   
 end
